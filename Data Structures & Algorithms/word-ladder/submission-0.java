f797c0d0d58128287b0ef1cf5e2da1b3c48acdab
Mingran Xu <106843013+kevinmingranxu03@users.noreply.github.com>
2026-07-16 00:20:44 -0700

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        if (!set.contains(endWord)) {
+            return 0;
+        }
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                if (word.equals(endWord)) {
+                    return step;
+                }
+                char[] arr = word.toCharArray();;
+                for (int j = 0; j < arr.length; j++) {
+                    char original = arr[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        arr[j] = c;
+                        String newWord = new String(arr);
+                        if (set.contains(newWord)) {
+                            queue.add(newWord);
+                            set.remove(newWord);
+                        }
+                    }
+                    arr[j] = original;
+                }
+            }
+            step++;
+        }
+        return 0;
+    }
+}
