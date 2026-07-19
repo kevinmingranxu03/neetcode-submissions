3aeacf50f18eb4b7dbb125bf39169c1956589bc8
Mingran Xu <106843013+kevinmingranxu03@users.noreply.github.com>
2026-07-19 00:05:19 -0700

    Add: foreign-dictionary - submission-0

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-0.java
@@ -0,0 +1,58 @@
+class Solution {
+    public String foreignDictionary(String[] words) {
+        Map<Character, Set<Character>> graph = new HashMap<>();
+        Map<Character, Integer> indegree = new HashMap<>();
+        // intialized all characters
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                graph.putIfAbsent(c, new HashSet<>());
+                indegree.putIfAbsent(c, 0);
+            }
+        }
+
+        // build adjacent word graph
+        for (int i = 0; i < words.length - 1; i++) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+            if (word1.length() > word2.length() && word1.startsWith(word2)) {
+                return "";
+            }
+            int len = Math.min(word1.length(), word2.length());
+            for (int j = 0; j < len; j++) {
+                char c1 = word1.charAt(j);
+                char c2 = word2.charAt(j);
+                if (c1 != c2) {
+                    // c1 must come before c2
+                    if (!graph.get(c1).contains(c2)) {
+                        graph.get(c1).add(c2);
+                        indegree.put(c2, indegree.get(c2) + 1);
+                    }
+                    break;
+                }
+            }
+        }
+            // kahn's algorithm, topologic sorting
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : indegree.keySet()) {
+            if (indegree.get(c) == 0) {
+                queue.offer(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            char cur = queue.poll();
+            sb.append(cur);
+            for (char neighbor : graph.get(cur)) {
+                indegree.put(neighbor, indegree.get(neighbor) - 1);
+                if (indegree.get(neighbor) == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        // if doesn't contain all characters, it exists cycle
+        if (sb.length() != indegree.size()) {
+            return "";
+        }
+        return sb.toString();
+    }
+}
